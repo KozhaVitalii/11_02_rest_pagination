3fa4ab57a59f54f1f1fce79b09d6036161aff683
Vitalii Kozha <116120008+KozhaVitalii@users.noreply.github.com>
2023-05-29 00:36:03 +0300

    added_comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// import articlesTpl from './templates/articles.hbs';
+import './css/common.css';
+import NewsApiService from './js/news-service'; // импортируем логику взаимодейтвия с API
+// import LoadMoreBtn from './js/components/load-more-btn';
+
+
 /*
  * - Пагинация
  *   - страница и кол-во на странице
@@ -9,54 +15,279 @@
  *
  * https://newsapi.org/
  * 4330ebfabc654a6992c2aa792f3173a3
+ * cdbc22a4fd484d829814de4916e0ca00 // зарегился и добавил свой ключ
  * http://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1
  */
 
-import articlesTpl from './templates/articles.hbs';
-import './css/common.css';
-import NewsApiService from './js/news-service';
-import LoadMoreBtn from './js/components/load-more-btn';
 
+// const options = {
+//   headers: {
+//     Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+// };
+
+// fetch('https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1', options)
+//   .then(r => r.json())
+//   .then(console.log); // отвыкаем выводить в консоль т.к. у нас есть вкладка network
+
+// также для примера подключусь вторым способом так как говорит об этом документация при регистрации, так тоже ок:
+// fetch('https://newsapi.org/v2/everything?q=cat&apiKey=cdbc22a4fd484d829814de4916e0ca00')
+//   .then(r => r.json())
+//   .then(console.log); // отвыкаем выводить в консоль т.к. у нас есть вкладка network
+
+// Теперь приступим к загрузке страниц, начнем с простого:
+
+// const options = {
+//   headers: {
+//     Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+// };
+// const url = 'https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1'; // вынесем в отдельную перемнную,
+// // чтобы наш код был аккуратнее
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log); 
+
+// Далее при сабмите формы, должен выполняться запрос с поиском того что мы ввели в инпут формы:
+
+// Для этого в нашей HTML уже есть разметка формы:
+    // <form class="form-inline search-form js-search-form">
+    //   <div class="form-group mx-sm-3 mb-2">
+    //     <label for="searchQuery" class="sr-only">Что ищем?</label>
+    //     <input
+    //       type="text"
+    //       class="form-control"
+    //       name="query"
+    //       id="searchQuery"
+    //       placeholder="Что ищем?"
+    //       autocomplete="off"
+    //     />
+    //   </div>
+    //   <button type="submit" class="btn btn-primary mb-2">Искать</button>
+    // </form>
+
+//  У нас есть ul куда мы будем вставлять наш наблон разметки для динамической загрузки карточек:
+    // <ul class="articles js-articles-container"></ul>
+
+// У нас есть кнопка загрузить ещё, чтобы подгружать доп. группу карточек:    
+    // <button
+    //   type="button"
+    //   class="btn btn-primary button"
+    //   data-action="load-more"
+    // >
+    //   <span
+    //     class="spinner-border spinner-border-sm spinner is-hidden"
+    //     role="status"
+    //     aria-hidden="true"
+    //   ></span>
+
+    //   <span class="label">Показать ещё</span>
+    // </button>
+    
+// настроим рефы:
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
   articlesContainer: document.querySelector('.js-articles-container'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]')
 };
-const loadMoreBtn = new LoadMoreBtn({
-  selector: '[data-action="load-more"]',
-  hidden: true,
-});
-const newsApiService = new NewsApiService();
 
+// Вешаем слушатель событий на форму и добавляем обработчик событий:
 refs.searchForm.addEventListener('submit', onSearch);
-loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+// let searchQuery = ''; // сделаем эту переменную глобальной, чтобы использовать в функциях onSearch() и onLoadMore,
+// но это не лучшее решение, ниже об этом описал (далее вынесем логику взаимодейтсвия с API в отдельный файл) Можем
+// убирать т.к. добавили геттер и сеттер в файле с логикой API
+
+// Добавляем функцию обработчик событий:
+// function onSearch(e) {
+//   e.preventDefault(); // убераем перезагрузку страницы при сабмите
+
+// // код который мы описали выше для загрузки карточек из бекенда, вставляем в нашу функцию.Такой вариант не очень, но для начала
+// // сделаем так:
+//   const options = {
+//   headers: {
+//     Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+// };
+// const url = 'https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1'; // вынесем в отдельную перемнную,
+// // чтобы наш код был аккуратнее
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
+
+// }
+
+// Работает, пока это все выводится на вкладку network.Далее хотелось бы значение которое необходимо искать, прописывать
+// не в коде, как "?q=cat", а получать как значение из инпута (т.е. то что вводит нам пользователь) для этого достучимся
+// до нашего инпута, заберем из него значение и подставим в наш запрос на бекенд:
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   searchQuery = e.currentTarget.elements.query.value; // получаем ссылку на форму через станартный для формы
+//   // атрибут name = "query". Т.е мы взяли форму "e.currentTarget" у неё свойство .elemments у неё свойство .query и
+//   // получили его значение .value
+
+//   const options = {
+//   headers: {
+//     Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+//   };
+
+// //  далее подставим динамическое значение для поиска из переменной searchQuery вместо нашего cat:
+// const url = 'https://newsapi.org/v2/everything?q=${searchQuery}&language=en&pageSize=5&page=1';
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
+
+// }
+
+// Ок, теперь добавим функциоал позволяющий загружать статьи по кнопке "загрузить ещё"
+// Первым делом настроим реф с нашей кнопкой в разметке, чтобы не дублировать код подымимся выше и добавим такую запись
+// в уже существующие рефы: loadMoreBtn: document.querySelector('[data-action="load-more"]'), а такж добавим слушатель
+// событий на эту кнопку и обработчик событий функцию onLoadMore (см. выше по тексту)
+
+// В эту функцию передаем тот же код что и для функции onSearch(), но есть проблема: как внутри этой функции получить
+// значение const searchQuery, если эта переменная объявлена внутри функции onSearch(). Получается, что при сабмите формы
+// необходимо где-то сохранять текущее значение нашей формы (значения инпута). Самое простое решение, это сделать нашу
+// переменную const searchQuery глобальной и вынести её за пределы функции и по умолчанию селать пустой: let searchQuery = '';
+// Вроде такое решение ок, но есть проблема, она заключается в том, что начинаютя макароны, а именно мы начинаем путать нашу
+// логику работы с API и логику отрисовки и реагирования на интерфейс. Ранее мы говорили, что функции которые что-то забирают
+// и которые что-то рисуют и обрабатывают должны быть разделены на разные функции. Соответсвенно нам логику работы с API
+// необходимо вынести в отдельный файл
+
+// function onLoadMore() {
+//    e.preventDefault();
+
+//   const options = {
+//   headers: {
+//     Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+//   };
+
+// const url = 'https://newsapi.org/v2/everything?q=${searchQuery}&language=en&pageSize=5&page=1';
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
+
+// }
+
+// Ок, далее всю логику работы с API просто вынесем в отдельный файл. Теперь перепишем наши функции onSearch() и onLoadMore
+// с учетом того, что мы вынесли нашу логику взаимодействия с API в отдельный файл (перед этим импортируем нашу логику которую
+// мы прописали в файле news-ServiceWorker.js) и создадим экземпляр класса, чтобы получить объект с методами и логикой, которую
+// описали для взаимодействия с API:
+
+const newsApiService = new NewsApiService(searchQuery); // таким образом мы достучались к той логике которую вынесли в отдельный файл,
+// т.е. у этого класса теперь метод fetchArticles()
+// console.log(newsApiService);
+
+
+// function onSearch(e) {
+//   e.preventDefault(); 
+
+//   searchQuery = e.currentTarget.elements.query.value;
+  
+//   newsApiService.fetchArticles(searchQuery); // благодаря тому что мы импортировали логику из файла взаимодействия с API (где 
+//   // мы создали прототип) + тому что мы объявили этот прототип(класс) здесь в коде перед функцией, мы вызываем этот класс и метод
+//   // который в него вложили, кроме того нам необходимо на вызванном методе достучаться к значению инпута для этого в параметр
+//   // функции вложим searchQuery и поместим значение переменной в наш экземпляр класса, так: new NewsApiService(searchQuery) 
+// }
+
+// // В этой функции также вызовим функцию оторую помистили в наш экземпляр класса:
+// function onLoadMore() {
+//   newsApiService.fetchArticles(searchQuery);
+// }
+
+// Проверяем, все работает. Проблема в том, что в нашем основном файле js все ещё есть наша глобальная
+// переменная let searchQuery = ''; при этом логику мы вынесли в отдельный файл.Логично, что именно он должен запоминать
+// значение которое будет поступать в searchQuery. Соответственно перенесем эту переменную туда, но немного измени логику
+// и перепишем наш экземпляр класса (см. в файле логики работы с апи), ну и после доработки класса, перепишем наши функции:
 
 function onSearch(e) {
-  e.preventDefault();
+  e.preventDefault(); 
 
-  newsApiService.query = e.currentTarget.elements.query.value;
-
-  if (newsApiService.query === '') {
-    return alert('Введи что-то нормальное');
-  }
-
-  loadMoreBtn.show();
-  newsApiService.resetPage();
-  clearArticlesContainer();
-  fetchArticles();
+  newsApiService.query = e.currentTarget.elements.query.value; // что мы сделали? в этой строке кода, при сабмите формы, мы
+  // на наш объект const newsApiService = new NewsApiService(searchQuery); в его свойство this.searchQuery, которое лежит в
+  // constructor() { this.searchQuery = ''; } при помощи сеттера: set query(newQuery) { this.searchQuery = newQuery; }, 
+  // записали то что мы из формы получаем: "= e.currentTarget.elements.query.value;" (кота, собаку и т.д.)
+  newsApiService.resetPage(); // когда мы сабмитим форму, мы устанавливаем значение для нашего searchQuery, здесь
+  // newsApiService.query = e.currentTarget.elements.query.value; поэтому сразу же добавим нашу функцию ресет. Теперь после
+  // каждого сабмита, наш пейдж будет = 1, при этом по кнопке "показать ещё" наш пейдж по прежнему будет увеличиваться на +1
+  // Т.е.если мы сабмитим повторно, то скорее всего мы ввели в наш инпут новое значение, поэтому начинаем наш пейдж снова с 1,
+  // даже если пользователь повторно нажмет на сабмит и значение в инпуте останется прежним, то резуьльтат запроса выдаст первую 
+  // страницу объектов
+  newsApiService.fetchArticles(searchQuery); 
 }
 
-function fetchArticles() {
-  loadMoreBtn.disable();
-  newsApiService.fetchArticles().then(articles => {
-    appendArticlesMarkup(articles);
-    loadMoreBtn.enable();
-  });
+
+function onLoadMore() {
+  newsApiService.fetchArticles(searchQuery);
 }
 
-function appendArticlesMarkup(articles) {
-  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
-}
+// Но даже после всех наших доработок проблема все ещё остается.В чем она ? Когда мы делаем запрос, к нам приходит результат
+// из первого пакета объектов(котиков), но когда мы нажимаем на кнопку "показать ещё", запрос выполняется, но в запросе
+// параметр page остается все ещё "1", т.е. получается что необходимо при каждом следующем запросе, увеличивать значение
+// page на 1. Необходимо доабать логику взаимодействия с бекендом (API). Соответственно понадобитья где-то хранить значение
+// нашего параетра page, для этого расширим конструктор нашего класса и скорректируем нашу строку запроса подставив
+// динамическое значение.
 
-function clearArticlesContainer() {
-  refs.articlesContainer.innerHTML = '';
-}
+
+
+
+
+
+// Оригинальный код:
+
+// import articlesTpl from './templates/articles.hbs';
+// import './css/common.css';
+// import NewsApiService from './js/news-service';
+// import LoadMoreBtn from './js/components/load-more-btn';
+
+// const refs = {
+//   searchForm: document.querySelector('.js-search-form'),
+//   articlesContainer: document.querySelector('.js-articles-container'),
+// };
+// const loadMoreBtn = new LoadMoreBtn({
+//   selector: '[data-action="load-more"]',
+//   hidden: true,
+// });
+// const newsApiService = new NewsApiService();
+
+// refs.searchForm.addEventListener('submit', onSearch);
+// loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   newsApiService.query = e.currentTarget.elements.query.value;
+
+//   if (newsApiService.query === '') {
+//     return alert('Введи что-то нормальное');
+//   }
+
+//   loadMoreBtn.show();
+//   newsApiService.resetPage();
+//   clearArticlesContainer();
+//   fetchArticles();
+// }
+
+// function fetchArticles() {
+//   loadMoreBtn.disable();
+//   newsApiService.fetchArticles().then(articles => {
+//     appendArticlesMarkup(articles);
+//     loadMoreBtn.enable();
+//   });
+// }
+
+// function appendArticlesMarkup(articles) {
+//   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+// }
+
+// function clearArticlesContainer() {
+//   refs.articlesContainer.innerHTML = '';
+// }
--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,32 +1,159 @@
-const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
-const BASE_URL = 'https://newsapi.org/v2';
-const options = {
-  headers: {
-    Authorization: API_KEY,
-  },
-};
+// Выносим нашу логику работы с API в это файл:
 
-export default class NewsApiService {
+// Создадим класс, который будет отвечать за взаимодействие с API: 
+// export default class NewsApiService { 
+// // создадим внутри него конструктор (как для любого класса):
+//   constructor() {
+  
+// }
+//   // создадим метод который будет отвечать за HTTP запросы, т.е. перенесем в него уже написанный нами код, который мы ранее
+//   // написали и поместили внутрь функции onSearch() и onLoadMore():
+//   fetchArticles(searchQuery) {
+//     const options = {
+//       headers: {
+//         Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+//   };
+
+//     const url = 'https://newsapi.org/v2/everything?q=${searchQuery}&language=en&pageSize=5&page=1'; 
+
+//     fetch(url, options)
+//       .then(r => r.json())
+//       .then(console.log); 
+//   } 
+  
+// }
+
+// Проверяем, все работает. Проблема в том, что в нашем основном файле js все ещё есть наша глобальная
+// переменная let searchQuery = ''; при этом логику мы вынесли в отдельный файл.Логично, что именно он должен запоминать
+// значение которое будет поступать в searchQuery. Соответственно перенесем эту переменную туда, но немного измени логику
+// и перепишем наш экземпляр класса:
+
+// export default class NewsApiService { 
+// // создадим внутри него конструктор (как для любого класса):
+//   constructor() {
+//     this.searchQuery = ''; // так вынесем нашу переменную в конструктор класса и далее будем подставлять её динамически 
+//     // в нашу стороку запроса
+//     this.page = 1; // храним первоначальное значение параметра запроса
+//   }
+//   // создадим метод который будет отвечать за HTTP запросы, т.е. перенесем в него уже написанный нами код, который мы ранее
+//   // написали и поместили внутрь функции onSearch() и onLoadMore():
+//   fetchArticles(searchQuery) {
+//     const options = {
+//       headers: {
+//         Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+//   };
+
+//     const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`; 
+
+//     fetch(url, options)
+//       .then(r => r.json())
+//       .then(console.log); 
+//   } 
+  
+// // Для того чтобы из внешнего кода что-то записать в свойство класса this.searchQuery = ''; создадим геттер и сеттер:
+
+//   // теперь значение свойства  this.searchQuery можно получить через геттер:
+//   get query() {
+//     return this.searchQuery;
+//   }
+// //  либо теперь через сеттер мы можем перезаписать новое значение поверх того значения, которое уже есть: 
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }  
+// }
+
+// Но даже после всех наших доработок проблема все ещё остается.В чем она ? Когда мы делаем запрос, к нам приходит результат
+// из первого пакета объектов(котиков), но когда мы нажимаем на кнопку "показать ещё", запрос выполняется, но в запросе
+// параметр page остается все ещё "1", т.е. получается что необходимо при каждом следующем запросе, увеличивать значение
+// page на 1. Необходимо доабать логику взаимодействия с бекендом (API). Соответственно понадобитья где-то хранить значение
+// нашего параетра page, для этого расширим конструктор нашего класса и скорректируем нашу строку запроса подставив
+// динамическое значение const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;. 
+// После чего останется добавить увеличение значения нашего this.page. Это необходимо сделать здесь fetch(url, options)
+// .then(r => r.json()).then(console.log);} т.к. именно сюда приходят данные. Пишем условие, если по результатам HTTP запроса 
+// результат успешный, то увеличиваем на 1:  
+
+// export default class NewsApiService { 
+//   constructor() {
+//     this.searchQuery = ''; 
+//     this.page = 1; // храним первоначальное значение параметра запроса
+//   }
+
+//   fetchArticles(searchQuery) {
+//     console.log('До запроса: ', this); // добавим консоль чтобы видеть как меняется значение при каждом обращении через HTTP запрос
+
+//     const options = {
+//       headers: {
+//         Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+//   },
+//   };
+
+//     const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`; 
+
+//     fetch(url, options)
+//       .then(r => r.json())
+//       .then(data => {
+//         console.log(data); // только лишь для того чтобы посмотреть в консоли что приходит по запросу
+//         this.page += 1; // при каждом успешном обращении к бекенду, увеличиваем значение page на +1
+//         console.log('После запроса, если все ок: ', this); // можно посмотреть в консоле как изменяется номер пейдж при 
+//         // каждом нажатии на кнопку "Показать ещё" 
+//       }); 
+//   } 
+  
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }  
+// }
+
+// Логика увеличения page такая: изначально как только мы создали наш const newsApiService = new NewsApiService(searchQuery)
+// наш searchQuery = "", а наш page = 1, далее когда мы в инпут вносим значение и нажимаем искать(сабмитим нашу форму), наше
+// значение(к примеру cat или dog) из инпута подставляется в newsApiService.query = e.currentTarget.elements.query.value; и
+// пошел HTTP запрос newsApiService.fetchArticles(searchQuery); и если в нем фетч выполнился успешно: fetch(url, options)
+// .then(r => r.json()).then(data => {});, то тогда мы увеличиваем наш пейдж на 1: then(data => {this.page += 1;});
+// Соответственно на следующий запрос(а следующий запрос у нас срабатывает когда мы нажимаем на кнопку "Показать ещё", т.к.мы
+// на эту кнопку повесили слушатель событий, которому указали функцию обработчик onLoadMore() в которую вложили функцию с нашей
+// основной логикой взаимодействия с бекендом newsApiService.fetchArticles(searchQuery)), наша пейдж уже будет с номером 2 и так
+// далее с каждым нажатием на кнопку "показать ещё" при успешном выполнении запроса, наш пейдж будет увеличиваться на +1
+
+// Можем переписать так:
+
+export default class NewsApiService { 
   constructor() {
-    this.searchQuery = '';
-    this.page = 1;
+    this.searchQuery = ''; 
+    this.page = 1; // храним первоначальное значение параметра запроса
   }
 
-  fetchArticles() {
-    const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+  fetchArticles(searchQuery) {
+    console.log('До запроса: ', this); // добавим консоль чтобы видеть как меняется значение при каждом обращении через HTTP запрос
 
-    return fetch(url, options)
-      .then(response => response.json())
-      .then(({ articles }) => {
-        this.incrementPage();
-        return articles;
-      });
-  }
+    const options = {
+      headers: {
+        Authorization: 'cdbc22a4fd484d829814de4916e0ca00',
+  },
+  };
 
+    const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`; 
+
+    fetch(url, options)
+      .then(r => r.json())
+      .then(data => {
+        this.incrementPage(); 
+        }); 
+  } 
+  
+  // Т.е. добавим небольшую функцию, в которую перенесем наше +1 и просто будем её вызывать в нашем фетч (так аккуратнее)
+
+  // Назовем функцию увеличения страницы так чтобы было понятна суть:
   incrementPage() {
     this.page += 1;
   }
 
+  // Аналогично назовем функцию по смыслу:
   resetPage() {
     this.page = 1;
   }
@@ -37,5 +164,58 @@ export default class NewsApiService {
 
   set query(newQuery) {
     this.searchQuery = newQuery;
-  }
+  }  
 }
+
+// Все работает, но в чем проблема ? Если мы в инпут вместо cat теперь введём dog и засабмитим форму(т.е.
+// нажмем на кнопку "искать"), наше свойство page не приит исходное значение "1", а продолжит нумерацию + 1 с того номера
+// на котором у нас закончилась нумерация страниц при поиске cat. По сути меняется значение searchQuery, но наш page не
+// сбрасывается. Чтобы это пофиксить необходимо делать ресет: resetPage() {this.page = 1;}. Определимся где это вызывать?
+// Когда мы сабмитим форму в функции onSearch(e), мы устанавливаем значение для нашего searchQuery, здесь 
+// searchQuery = e.currentTarget.elements.query.value; поэтому сразу же после можем добавить нашу функцию ресет.
+
+
+
+// Оригиальный код:
+
+// const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
+// const BASE_URL = 'https://newsapi.org/v2';
+// const options = {
+//   headers: {
+//     Authorization: API_KEY,
+//   },
+// };
+
+// export default class NewsApiService {
+//   constructor() {
+//     this.searchQuery = '';
+//     this.page = 1;
+//   }
+
+//   fetchArticles() {
+//     const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+
+//     return fetch(url, options)
+//       .then(response => response.json())
+//       .then(({ articles }) => {
+//         this.incrementPage();
+//         return articles;
+//       });
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+// }
